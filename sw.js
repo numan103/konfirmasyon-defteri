46e9f5849aae57af9342a31de93bd0172431748f
numan103 <numan103@users.noreply.github.com>
2026-08-11 17:28:33 +0300

    Alfa Edu'yu yalnizca trader kanallari sayfasina cevir (b15)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'alfa-v4';
-const BUILD_ID = 'b14';
+const BUILD_ID = 'b15';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
