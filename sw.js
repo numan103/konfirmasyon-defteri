c2c31170e546e390524737bd824608aa8274a55c
numan103 <numan103@users.noreply.github.com>
2026-08-11 21:26:50 +0300

    Alfa Designer: seffaf cam govde + ince beyaz dis cizgi, atletik siluet (b16)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'alfa-v4';
-const BUILD_ID = 'b15';
+const BUILD_ID = 'b16';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
