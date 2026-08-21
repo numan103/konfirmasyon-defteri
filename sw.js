21736e14ab04ecba7f92a1a6d87277158c5e142b
numan103 <numan103@users.noreply.github.com>
2026-08-21 22:18:35 +0300

    b70: sell history shows buy price + profit/loss

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿const CACHE = 'alfa-v28';
-const BUILD_ID = 'b69';
+const BUILD_ID = 'b70';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
