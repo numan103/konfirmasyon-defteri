2df50340a84202151a4f7de75c476f6f55cef35f
numan103 <numan103@users.noreply.github.com>
2026-08-22 11:54:24 +0300

    b74: remove sale amount from sell history, show only profit

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 ﻿const CACHE = 'alfa-v29';
-const BUILD_ID = 'b73';
+const BUILD_ID = 'b74';
 self.addEventListener('install', e => { self.skipWaiting(); });
 self.addEventListener('activate', e => {
   e.waitUntil(
